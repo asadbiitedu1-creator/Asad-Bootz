f2abbc1b0e1fa4646c4a409563ab7a07547c2c24
asadbiitedu1-creator <asad.biit.edu1@gmail.com>
2025-08-16 16:45:39 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -38,7 +38,7 @@ async (conn, mek, m, { from }) => {
 ┃◈┃• *Version*: 2.0.0 Beta
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐀𝗌αᑯ_𝐌𝐃 ❣️*`, // Display the owner's details
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝗔𝘀𝗮𝗱_𝐌𝐃 ❣️*`, // Display the owner's details
             contextInfo: {
                 mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
                 forwardingScore: 999,
